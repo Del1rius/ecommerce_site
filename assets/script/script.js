e1f3a405101d7025086cb838f15732a3257459d8
Del1rius <timothy.aidan.barry@gmail.com>
2023-05-30 11:50:22 +0200

    Home Page Complete

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,30 @@
- let product = { 
-    name: "Alienware Laptop1",
-    price: "R10000",
-    
- }
+let products = [
+  {
+    productImg: "https://i.postimg.cc/6qgD35pG/laptop2.webp",
+    name: "Alienware Laptops",
+    description: "Alienware's Elite Lineup of laptops.",
+  },
+  {
+   productImg: "https://i.postimg.cc/4xJCcXM9/monitor.jpg",
+    name: "Alienware Monitors",
+    description: "The Best Gaming Monitors in the Industry.",
+  },
+  {
+    productImg: "https://i.postimg.cc/jSvxvPgx/desktop2.webp",
+    name: "Alienware Desktops",
+    description:
+      "The Best Experience for High-End Gaming.",
+  },
+];
+
+let dispProducts = document.querySelector("#featuredProducts");
+products.forEach((data) => {
+  dispProducts.innerHTML += `
+      <div class="card col-3 bg-black text-white border-white mx-auto p-2 text-center">
+      <img src="${data.productImg}">
+      <h4 class="display-6">${data.name}</h4>
+      <div class="card-body">
+      <p class="text-white fs-4">${data.description}</p>
+      </div>
+    </div>`;
+});
